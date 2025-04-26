4f6bba42d31bb60b8064083ab24145cdb272ac42
Гром Алексеев <gromovl.love@gmail.com>
2025-04-26 22:51:07 +0500

    Правильный путь к статике + добавил burger.js

--- chernoha/settings.py
+++ chernoha/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
 
 # Статика
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'main' / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
--- staticfiles/main/js/burger.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    const aside = document.querySelector('aside');
-    aside.classList.toggle('open');
-}
